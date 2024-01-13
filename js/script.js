bc407bb52cf818e8abbfea25cf3f2cb9875f6c2a
Soma_Ftees <135269960+somaftees@users.noreply.github.com>
2024-01-13 22:30:28 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+//////////////////////////////////////////////////
     window.onscroll = function() {scrollFunction()};
 
     function scrollFunction() {
@@ -17,3 +18,49 @@
     function googleTranslateElementInit() {
         new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'ar,en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
       }
+
+      function validateForm(event) {
+        event.preventDefault(); // يمنع النموذج من إرسال البيانات تلقائيًا
+    
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+    
+        // قم بإجراء التحقق من صحة المدخلات هنا
+        if (username === "" || password === "") {
+          alert("يرجى تعبئة جميع الحقول");
+          return false;
+        }
+        else if (username === "tasnim" && password === "1234"){
+        // توجيه المستخدم إلى الصفحة المطلوبة
+        window.location.href = "insert.html";
+        }
+       else if (username === "soma" && password === "12345"){
+        // توجيه المستخدم إلى الصفحة المطلوبة
+        window.location.href = "insert.html";
+      }
+      else
+      alert("اسم المستخدم او كلمة المرور خاطئان")
+      }
+
+      document.getElementById("productForm").addEventListener("submit", function(event) {
+        event.preventDefault(); // منع إرسال النموذج
+  
+        // استخراج قيم المدخلات
+        var productName = document.getElementById("productName").value;
+        var productPrice = document.getElementById("productPrice").value;
+        var productImage = document.getElementById("productImage").value;
+  
+        // قم بإجراء التحقق من صحة المدخلات هنا
+        if (productName === "" || productPrice === "" || productImage === "") {
+          alert("يرجى تعبئة جميع الحقول");
+          return;
+        }
+  
+        // معالجة البيانات هنا
+  
+        // إعادة تعيين النموذج
+        document.getElementById("productForm").reset();
+  
+        // عرض رسالة للتأكيد أو إعادة التوجيه إلى صفحة أخرى
+        alert("تمت إضافة المنتج بنجاح!");
+      });
